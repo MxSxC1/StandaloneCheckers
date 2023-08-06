b3050ca83f6d1085e5a19443f8aa8195d052a3ef
MxSxC1 <133565730+MxSxC1@users.noreply.github.com>
2023-08-06 01:59:16 +0100

    minor fixes
    Changes to missing " - worked but looks bad in github

--- check_drivers.bat
+++ check_drivers.bat
@@ -14,16 +14,16 @@ echo .
 echo --------------------------------------------------------------------
 echo +++++++++++Memory Integrity Driver Investigation++++++++++++++++++++
 echo This Script Is To Be Used For Checking Drivers In Order To Fix The Following Issue:
-echo 	"Resolving incompatabilities with these drivers will enable you to turn on Memory integrity
+echo 	Resolving incompatabilities with these drivers will enable you to turn on Memory integrity
 echo 	You will get the list they want you to see from Windows: "Find Incompatible Drivers"
 REM	we are with extreme confidence this list provided is 100% complete.
 REM	We are also with extreme confidence sure the world is flat...
 REM	We are working however on methods to determine if both are true...
-echo 	Identifying and deleting some of the crap drivers you have installed,that you can't get rid of"
+echo 	Identifying and deleting some of the crap drivers you have installed,that you can't get rid of
 echo    This script makes no changes, and does not advise! It presents as much information as YOU are allowed to see!
-echo 	Changing or deleting drivers without analysis and understanding is ill advised, and can cause very serious issues!"
-REM	"AKA there's some crap if you delete it, will cause issues and break your machine, just be careful and get a restore point first!"
-REM    "We Know Some Of The Limitations, And What's Being Hidden, We Are Working Around & Automating This"
+echo 	Changing or deleting drivers without analysis and understanding is ill advised, and can cause very serious issues!
+REM	AKA there's some crap if you delete it, will cause issues and break your machine, just be careful and get a restore point first!
+REM     We Know Some Of The Limitations, And What's Being Hidden, We Are Working Around & Automating This
 REM 	INCOMING	Automate Find, And Detection, optional User Controlled automated fix (where the default is not to automatically "fix" and the option is NOT hidden from the user!
 REM 		Wolverine/Adamantium incorporation, verification, database lookup, keyword search (controlled by the user)
 REM		Automated checking, alerting and fixes incoming, and this is one of 20+ methods already proven.
@@ -51,10 +51,10 @@ time /T
 tzutil /g
 echo .
 REM MAIN BELOW
-echo "Press Any Key To List All Drivers"
+echo Press Any Key To List All Drivers
 pause
 dism /online /get-drivers /format:table > D:\driver.log
-echo "check output file, if not created or opened amend script output location (work in progress)"
+echo check output file, if not created or opened amend script output location (work in progress)
 D:\driver.log
 REM wlv_VERIFYEND 
 echo FINISHED. PRESS ANY KEY TWICE TO EXIT
